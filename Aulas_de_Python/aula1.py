d59b48cb748fec7a73fe95948a9a75a6c523a7d0
AdilarMaia <adilar_maia@yahoo.com.br>
2021-10-30 04:44:12 -0300

    Primeiro teste Python
    Código simples para teste de git e github

---
+++ Aulas_de_Python/aula1.py
@@ -0,0 +1,10 @@
+
+# TESTE DIDÁTICO SÓ PRA TESTE DO GIT E GITHUB
+
+print('Olá! Meu nome é Robert')
+nome = str(input('Diga o seu nome:'))
+idade = int(input('Diga a sua idade:'))
+local = str(input('Diga aonde mora:'))
+peso = float(input('Diga o seu peso:'))
+print('Legal {} seja bem vindo à {}'.format(nome, local))
+print('O seu peso {} é igual ao meu e eu sou mais novo que sua idade {}'.format(peso, idade))
